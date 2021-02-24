5ccb4d0c1d8930fd4c90d271ac40c825d30ac681
seok <nova9353@gmail.com>
2021-02-24 22:09:08 +0900

    Add MemberController And MemberForm

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        /* Member를 사용해도 되지만 MemberForm을 만드는 이유
+         * Member에는 Member Table에 필요한 정보를 넣어 주지만
+         * MemberForm에서는 약관등 Member Table말고도 다른 정보들이 들어 올 수 있기 때문
+         */
+        System.out.println(form.getName());
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
